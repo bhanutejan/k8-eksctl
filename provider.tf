7bb6cacfc2d536792f8562a8e5008d58ae765650
= <=>
2024-09-17 18:42:33 +0530

    committing changes

---
+++ provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+
+
+# backend "s3" {
+#   bucket = "teja-remote-state"
+#   key = "eksctl"
+#   region = "us-east-1"
+#   dynamodb_table = "teja-locking"
+# }
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
